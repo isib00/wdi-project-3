d5556de17f4c8f6054057942f591c7f7c985b9a6
Joe Byrne <joesphbyrne2@gmail.com>
2018-11-16 15:49:03 +0000

    homepage and aboutpage

--- config/router.js
+++ config/router.js
@@ -1,3 +1,13 @@
 //hey this is anais
+// console.log('hey guys! this is the best group ever')
 
-console.log('hey guys! this is the best group ever')
+//Joe's itemIndex route
+
+const router = require('express').Router();
+
+const itemsController = require('../controllers/itemsController');
+
+router.route('/items')
+  .get(itemsController.index);
+
+module.exports = router;
