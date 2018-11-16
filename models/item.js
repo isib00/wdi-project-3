a1fc55718a518df000b8136560dbc6fa96d87ff9
isib00 <anais.berkes@gmx.de>
2018-11-16 16:02:05 +0000

    added seeds

--- models/item.js
+++ models/item.js
@@ -2,21 +2,24 @@ const mongoose = require('mongoose');
 
 const itemSchema = mongoose.Schema({
   itemName: { type: String, required: true },
-  seller: String,
   imageUrl: String,
-  catgories: Array,
+  seller: String,
+  //assign an array of categories to every item
+  catgories: [String],
   price: Number,
-  rating: Number,
-  uploadDate: Date,
+  // rating: Number ---> let us do that after we have reached MVP: it's complicated
+  uploadDate: { type: Date, default: Date.now },
   location: String,
   sizes: String,
   colour: String,
+  //Users can comment on items
   comments: [
     {
       text: String,
       user: { type: mongoose.Schema.ObjectId, ref: 'User' }
     }
   ],
+  //links the item to the seller's id:
   createdBy: {
     type: mongoose.Schema.ObjectId,
     ref: 'User'
