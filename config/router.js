efb26cd8abd824b103e102e9fcc3ce507b7e18c5
isib00 <anais.berkes@gmx.de>
2018-11-18 23:02:28 +0000

    needed to commit to create a new branch

--- config/router.js
+++ config/router.js
@@ -1,12 +1,30 @@
 const router = require('express').Router();
 const itemController = require('../controllers/itemController');
 const authController = require('../controllers/authController');
+const commentController = require('../controllers/commentController');
+const userController = require('../controllers/userController');
 
 
 // console.log('hey guys! this is the best group ever')
 
 
 //Anais's side
+router.route('/items')
+  .post( itemController.create);
+
+router.route('/items/:id')
+  .get(itemController.show)
+  .put( itemController.update)
+  .delete( itemController.delete);
+
+router.route('/items/:itemId/comments')
+  .post( commentController.create);
+
+router.route('/items/:itemId/comments/:commentId')
+  .delete( commentController.delete);
+
+router.route('/users/:id')
+  .get(userController.show);
 
 
 //Joe's side
---
+++ controllers/commentController.js
@@ -0,0 +1,29 @@
+const Item = require('../models/item');
+function createRoute(req, res, next) {
+  Item.findById(req.params.itemId)
+    .then(item => {
+      req.body.user = req.tokenUserId;
+      item.comments.push(req.body);
+      return item.save();
+    })
+    .then(item => Item.populate(item, 'createdBy comments.user'))
+    .then(item => res.json(item))
+    .catch(next);
+}
+
+function deleteRoute(req, res, next) {
+  Item.findById(req.params.itemId)
+    .then(item => {
+      const comment = item.comments.id(req.params.commentId);
+      comment.remove();
+      return item.save();
+    })
+    .then(item => Item.populate(item, 'createdBy comments.user'))
+    .then(item => res.json(item))
+    .catch(next);
+}
+
+module.exports = {
+  create: createRoute,
+  delete: deleteRoute
+};
