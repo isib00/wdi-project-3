1e22007e72dcd3894a7bab346a53e3415775db45
Joe Byrne <joesphbyrne2@gmail.com>
2018-11-20 11:00:05 +0000

    login/register and authentication

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 
 const userSchema = mongoose.Schema({
@@ -20,6 +21,21 @@ const userSchema = mongoose.Schema({
     }
   ]
 });
+
+userSchema.pre('save', function() {
+  this.password = bcrypt.hashSync(this.password, 8);
+});
+
+userSchema.methods.validatePassword = function(attemptedPassword) {
+  return bcrypt.compareSync(attemptedPassword, this.password);
+};
+
+
+
+userSchema.set('toJSON', {
+  virtuals: true
+});
+
 
 //Get all the items that a seller has uploaded:
 // userSchema.virtual('itemsForSale', {
--- src/controllers/loginCtrl.js
+++ src/controllers/loginCtrl.js
@@ -1,21 +1,40 @@
-function loginCtrl($scope, $state, $auth, Flash) {
+function loginCtrl($scope, $state, $auth) {
   $scope.handleLogin = function() {
+    console.log('Logging in!');
     $auth.login($scope.user)
-      .then(result => {
-      // This is a flash message. It disappears after a few seconds.
-      // The first argument is the type:
-      // success, danger, warning, info
-      // The second argument is the message to show. It can be any string.
-        Flash.create('success', result.data.message);
-        $state.go('itemsIndex');
-      })
-      .catch(err => {
-      // NOTE: If login fails, show whatever message comes from the server.
-      //       Try logging in incorrectly, and check out the console log.
-        console.log('err is', err);
-        Flash.create('danger', 'Login failed: ' + err.data.message);
-      });
+      .then(() => $state.go('itemsIndex'))
+      .catch(err => console.log('There was an error', err));
   };
 }
 
+
 export default loginCtrl;
+
+
+
+
+
+
+
+
+
+
+// function loginCtrl($scope, $state, $auth, Flash) {
+//   $scope.handleLogin = function() {
+//     $auth.login($scope.user)
+//     .then(result => {
+//       // This is a flash message. It disappears after a few seconds.
+//       // The first argument is the type:
+//       // success, danger, warning, info
+//       // The second argument is the message to show. It can be any string.
+//       Flash.create('success', result.data.message);
+//       $state.go('itemsIndex');
+//     })
+//     .catch(err => {
+//       // NOTE: If login fails, show whatever message comes from the server.
+//       //       Try logging in incorrectly, and check out the console log.
+//       console.log('err is', err);
+//       Flash.create('danger', 'Login failed: ' + err.data.message);
+//     });
+//   };
+// }
